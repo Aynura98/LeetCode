477f0ccfd9144364c715e99079118d0eb3d7db33
Aynura Rustemova <aynura98rustemova@gmail.com>
2024-03-15 10:52:58 +0100

    Solution for Isomorphic strings problem

---
+++ Isomorphic_strings.py
@@ -0,0 +1,37 @@
+'''
+Example 1:
+
+Input: s = "egg", t = "add"
+Output: true
+Example 2:
+
+Input: s = "foo", t = "bar"
+Output: false
+Example 3:
+
+Input: s = "paper", t = "title"
+Output: true'''
+class Solution(object):
+    def isIsomorphic(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        
+        map1 = []
+        map2 = []
+        for idx in s:
+            map1.append(s.index(idx))
+        for idx in t:
+            map2.append(t.index(idx))
+        if map1 == map2:
+            return True
+        return False
+
+
+
+if __name__ == "__main__":
+    s =  Solution()
+    result = s.isIsomorphic(s = "badc", t = "baba")
+    print(result)   

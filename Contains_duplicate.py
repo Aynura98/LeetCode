99f181297dc476f1348307ce00b59d9248a420c1
Aynura Rustemova <aynura98rustemova@gmail.com>
2024-03-15 12:52:01 +0100

    Solution for Contains duplicates problem

--- Contains_duplicate.py
+++ Contains_duplicate.py
@@ -4,4 +4,17 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
+        new_set = set()
         
+        for i in nums:
+            if i in new_set:
+                return True
+            else:
+                new_set.add(i)
+        return False
+
+
+if __name__ == "__main__":
+    s =  Solution()
+    result = s.containsDuplicate(nums = [1,1,1,3,3,4,3,2,4,2])
+    print(result)   

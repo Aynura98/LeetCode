b71b16c8755f12af05cee8fccd208b0e578ae484
Aynura Rustemova <aynura98rustemova@gmail.com>
2024-03-16 21:46:27 +0100

    Solution for Power of two problem

--- Power_of_two.py
+++ Power_of_two.py
@@ -4,9 +4,15 @@ class Solution(object):
         :type n: int
         :rtype: bool
         """
-        
+        x = 0
+        while (pow(2,x) != n and x >= 0):
+            if pow(2,x) > n:
+                return False
+            x += 1 
+        return True
+
 
 if __name__ == "__main__":
     s =  Solution()
-    result = s.isPowerOfTwo(n = 1)
+    result = s.isPowerOfTwo(n = 3)
     print(result)   

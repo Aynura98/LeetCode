ed74d5188eea14c517706417c633d7371b1e135d
Aynura Rustemova <aynura98rustemova@gmail.com>
2024-03-16 22:06:41 +0100

    Solution for Valid anagram Problem

--- Valid_anagram.py
+++ Valid_anagram.py
@@ -5,8 +5,17 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
+        list = []
+        for i in range(len(t)):
+            list.append(t[i])
+        for i in range(len(s)):
+            if s[i] not in list or len(t) != len(s):
+                return False
+            else:
+                list.remove(s[i])
+        return True
         
 if __name__ == "__main__":
     s =  Solution()
-    result = s.isAnagram(s = "anagram", t = "nagaram")
+    result = s.isAnagram(s = "aacc", t = "ccac")
     print(result) 

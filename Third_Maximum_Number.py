48c1d21ebe50419d535ebb4780462e72fb7bd298
Aynura Rustemova <aynura98rustemova@gmail.com>
2024-04-08 10:57:33 +0200

    Solution for Third maximum problem

--- Third_Maximum_Number.py
+++ Third_Maximum_Number.py
@@ -4,8 +4,15 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        
-
+        new_list = []
+        nums.sort()
+        for i in range(len(nums)):
+            if nums[i] not in new_list:
+                new_list.append(nums[i])
+        if len(new_list) < 3:
+            return new_list[-1]
+        else:
+            return new_list[-3]
 if __name__ == "__main__":
     s =  Solution()
     result = s.thirdMax(nums = [2,2,3,1])
